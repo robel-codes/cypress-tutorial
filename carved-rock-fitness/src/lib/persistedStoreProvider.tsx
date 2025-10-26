6100d06aca8a096bc4d03a1b0cae1b8f10a6381f
Robel Gebremeskel <rofikre@yahoo.com>
2025-10-26 18:14:37 -0400

    exposed redux store to cypress tests

--- carved-rock-fitness/src/lib/persistedStoreProvider.tsx
+++ carved-rock-fitness/src/lib/persistedStoreProvider.tsx
@@ -7,6 +7,11 @@ import { persistor, store } from './store';
 
 export function PersistedStoreProvider({ children }: { children: ReactNode }) {
 
+  // Expose the store to Cypress tests
+  if (typeof window !== 'undefined' && window.Cypress) {
+    window.store = store;
+  }
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
