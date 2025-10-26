a40d7065b26eeb7ff7ce6a9e90365b47e22c8c86
Robel Gebremeskel <rofikre@yahoo.com>
2025-10-26 18:47:16 -0400

    added cypress-recurse plugin

--- carved-rock-fitness/cypress/support/e2e.ts
+++ carved-rock-fitness/cypress/support/e2e.ts
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-recurse/commands';
 
 Cypress.on("uncaught:exception", (err) => {
   // Cypress and React Hydrating the document don't get along
